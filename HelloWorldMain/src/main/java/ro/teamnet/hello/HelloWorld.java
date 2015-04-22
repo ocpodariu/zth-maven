560512d35957669d27e545d0b6d487f145c8e969
Ovidiu PODARIU <ocpodariu@yahoo.com>
2015-04-22 11:37:54 +0300

    Add Log4J and include debug/info messages

--- HelloWorldMain/src/main/java/ro/teamnet/hello/HelloWorld.java
+++ HelloWorldMain/src/main/java/ro/teamnet/hello/HelloWorld.java
@@ -1,22 +1,30 @@
 package ro.teamnet.hello;
 
+import org.apache.log4j.Logger;
+import org.junit.Test;
+
 /**
  * Author: Ovidiu
  * Date:   4/22/2015
  */
 public class HelloWorld {
 
+    static final Logger logger = Logger.getLogger(HelloWorld.class.getName());
+
     public void sayHello() {
+        logger.debug("DEBUG -> Enters in sayHello() method from HelloWorld");
         System.out.println("Hello World!");
     }
 
     public String returnHelloKey() {
+        logger.info("INFO -> Enters in returnHelloKey from HelloWorld");
         return "HelloKey";
     }
 
     public static void main(String[] args) {
         HelloWorld hello = new HelloWorld();
         hello.sayHello();
+        System.out.println(hello.returnHelloKey());
     }
 
 }
